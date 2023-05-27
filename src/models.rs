b35f17852fa71899dade39d736407c51fc9951b2
Mikkel Aas <mikkel.aas@schibsted.com>
2023-05-27 20:04:06 +0200

    wip: more fields

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,20 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Clan {
     tag: String,
+    name: String,
+    description: String,
+    members: i64,
+    r#type: ClanType,
+    #[serde(rename = "warLeague")]
+    war_league: WarLeague,
+    #[serde(rename = "capitalLeague")]
+    capital_league: CapitalLeague,
+    #[serde(rename = "memberList")]
+    member_list: Vec<ClanMember>,
     #[serde(rename = "clanLevel")]
     clan_level: i64,
     #[serde(rename = "warWinStreak")]
@@ -32,5 +44,97 @@ pub struct Clan {
     #[serde(rename = "requiredVersusTrophies")]
     required_versus_trophies: i64,
     #[serde(rename = "isWarLogPublic")]
-    is_war_log_public: bool
+    is_war_log_public: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ClanMember {
+    league: League,
+    #[serde(rename = "builderBaseLeague")]
+    builder_base_league: BuilderBaseLague,
+    #[serde(rename = "versusTrophies")]
+    versus_trophies: i64,
+    tag: String,
+    name: String,
+    role: ClanMemberRole,
+    #[serde(rename = "expLevel")]
+    exp_level: i64,
+    #[serde(rename = "clanRank")]
+    clan_rank: i64,
+    #[serde(rename = "previousClanRank")]
+    previous_clan_rank: i64,
+    donations: i64,
+    #[serde(rename = "donationsReceived")]
+    donations_received: i64,
+    trophies: i64,
+    #[serde(rename = "builderBaseTrophies")]
+    builder_base_trophies: i64,
+    #[serde(rename = "playerHouse")]
+    player_house: PlayerHouse,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct League {
+    name: String,
+    id: i64,
+    #[serde(rename = "iconUrls")]
+    icon_urls: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BuilderBaseLague {
+    name: String,
+    id: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WarLeague {
+    name: String,
+    id: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CapitalLeague {
+    name: String,
+    id: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerHouse {
+    elements: Vec<PlayerHouseElement>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerHouseElement {
+    id: i64,
+    r#type: PlayerHouseElementType,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ClanType {
+    #[serde(rename = "open")]
+    Open,
+    #[serde(rename = "invite_only")]
+    InviteOnly,
+    #[serde(rename = "closed")]
+    Closed,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ClanMemberRole {
+    #[serde(rename = "not_member")]
+    NotMember,
+    #[serde(rename = "member")]
+    Member,
+    #[serde(rename = "leader")]
+    Leader,
+    #[serde(rename = "admin")]
+    Admin,
+    #[serde(rename = "coLeader")]
+    CoLeader,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PlayerHouseElementType {
+    
 }
