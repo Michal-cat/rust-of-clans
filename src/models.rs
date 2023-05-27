0795ec73754ffc34d31066e61866e478fdd6faa5
Mikkel Aas <aa.mikkel2902@gmail.com>
2023-05-27 23:15:49 +0200

    make fields public

--- src/models.rs
+++ src/models.rs
@@ -5,119 +5,117 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Clan {
     #[serde(rename = "warLeague")]
-    war_league: WarLeague,
+    pub war_league: WarLeague,
     #[serde(rename = "capitalLeague")]
-    capital_league: CapitalLeague,
+    pub capital_league: CapitalLeague,
     #[serde(rename = "memberList")]
-    member_list: Vec<ClanMember>,
-    tag: String,
+    pub member_list: Vec<ClanMember>,
+    pub tag: String,
     #[serde(rename = "clanBuilderBasePoints")]
-    clan_builder_base_points: i64,
+    pub clan_builder_base_points: i64,
     #[serde(rename = "clanVersusPoints")]
-    clan_versus_points: i64,
+    pub clan_versus_points: i64,
     #[serde(rename = "warWins")]
-    war_wins: i64,
+    pub war_wins: i64,
     #[serde(rename = "warTies")]
-    war_ties: i64,
+    pub war_ties: i64,
     #[serde(rename = "warLosses")]
-    war_losses: i64,
+    pub war_losses: i64,
     #[serde(rename = "clanPoints")]
-    clan_points: i64,
+    pub clan_points: i64,
     #[serde(rename = "requiredTownhallLevel")]
-    required_town_hall_level: i64,
+    pub required_town_hall_level: i64,
     #[serde(rename = "chatLanguage")]
-    chat_language: Language,
+    pub chat_language: Language,
     #[serde(rename = "isFamilyFriendly")]
-    is_family_friendly: bool,
+    pub is_family_friendly: bool,
     #[serde(rename = "clanCapitalPoints")]
-    clan_capital_points: i64,
+    pub clan_capital_points: i64,
     #[serde(rename = "requiredTrophies")]
-    required_trophies: i64,
+    pub required_trophies: i64,
     #[serde(rename = "requiredBuilderBaseTrophies")]
-    required_builder_base_trophies: i64,
+    pub required_builder_base_trophies: i64,
     #[serde(rename = "requiredVersusTrophies")]
-    required_versus_trophies: i64,
+    pub required_versus_trophies: i64,
     #[serde(rename = "isWarLogPublic")]
-    is_war_log_public: bool,
+    pub is_war_log_public: bool,
     #[serde(rename = "warFrequency")]
-    war_frequency: WarFrequency,
+    pub war_frequency: WarFrequency,
     #[serde(rename = "clanLevel")]
-    clan_level: i64,
+    pub clan_level: i64,
     #[serde(rename = "warWinStreak")]
-    war_win_streak: i64,
-    labels: Vec<Label>,
-    name: String,
-    location: Location,
-
-    description: String,
-    members: i64,
-    r#type: ClanType,
-
+    pub war_win_streak: i64,
+    pub labels: Vec<Label>,
+    pub name: String,
+    pub location: Location,
+    pub description: String,
+    pub members: i64,
+    pub r#type: ClanType,
     #[serde(rename = "clanCapital")]
-    clan_capital: ClanCapital,
+    pub clan_capital: ClanCapital,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClanMember {
-    league: League,
+    pub league: League,
     #[serde(rename = "builderBaseLeague")]
-    builder_base_league: BuilderBaseLague,
+    pub builder_base_league: BuilderBaseLague,
     #[serde(rename = "versusTrophies")]
-    versus_trophies: i64,
-    tag: String,
-    name: String,
-    role: ClanMemberRole,
+    pub versus_trophies: i64,
+    pub tag: String,
+    pub name: String,
+    pub role: ClanMemberRole,
     #[serde(rename = "expLevel")]
-    exp_level: i64,
+    pub exp_level: i64,
     #[serde(rename = "clanRank")]
-    clan_rank: i64,
+    pub clan_rank: i64,
     #[serde(rename = "previousClanRank")]
-    previous_clan_rank: i64,
-    donations: i64,
+    pub previous_clan_rank: i64,
+    pub donations: i64,
     #[serde(rename = "donationsReceived")]
-    donations_received: i64,
-    trophies: i64,
+    pub donations_received: i64,
+    pub trophies: i64,
     #[serde(rename = "builderBaseTrophies")]
-    builder_base_trophies: i64,
+    pub builder_base_trophies: i64,
     #[serde(rename = "playerHouse")]
-    player_house: Option<PlayerHouse>,
+    pub player_house: Option<PlayerHouse>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct League {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
     #[serde(rename = "iconUrls")]
-    icon_urls: HashMap<String, String>,
+    pub icon_urls: HashMap<String, String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BuilderBaseLague {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WarLeague {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CapitalLeague {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PlayerHouse {
-    elements: Vec<PlayerHouseElement>,
+    pub elements: Vec<PlayerHouseElement>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PlayerHouseElement {
-    id: i64,
-    r#type: PlayerHouseElementType,
+    pub id: i64,
+    pub r#type: PlayerHouseElementType,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -160,10 +158,10 @@ pub enum PlayerHouseElementType {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Language {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
     #[serde(rename = "languageCode")]
-    language_code: String,
+    pub language_code: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -186,35 +184,35 @@ pub enum WarFrequency {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Label {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
     #[serde(rename = "badgeUrls")]
-    badge_urls: Option<HashMap<String, String>>,
+    pub badge_urls: Option<HashMap<String, String>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClanCapital {
     #[serde(rename = "capitalHallLevel")]
-    capital_hall_level: i64,
-    districts: Vec<ClanDistrictData>,
+    pub capital_hall_level: i64,
+    pub districts: Vec<ClanDistrictData>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClanDistrictData {
-    name: String,
-    id: i64,
+    pub name: String,
+    pub id: i64,
     #[serde(rename = "districtHallLevel")]
-    district_hall_level: i64,
+    pub district_hall_level: i64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Location {
     #[serde(rename = "localizedName")]
-    localized_name: Option<String>,
-    id: i64,
-    name: String,
+    pub localized_name: Option<String>,
+    pub id: i64,
+    pub name: String,
     #[serde(rename = "isCountry")]
-    is_country: bool,
+    pub is_country: bool,
     #[serde(rename = "countryCode")]
-    country_code: String,
+    pub country_code: String,
 }
