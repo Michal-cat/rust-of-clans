37b57f55876a325278c52d63811d6dfb8446ae83
Mikkel Aas <aa.mikkel2902@gmail.com>
2023-05-28 14:48:15 +0200

    add documentation

--- src/requests.rs
+++ src/requests.rs
@@ -5,9 +5,29 @@ use reqwest::StatusCode;
 use urlencoding::encode;
 
 impl CoCClient {
+    /// Retrieves information about a Clash of Clans clan.
+    ///
+    /// # Arguments
+    ///
+    /// * `clan_tag` - The tag of the clan to retrieve information for.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` containing the `Clan` information if successful, or an error of type `CoCClientError`.
+    ///
+    /// # Errors
+    ///
+    /// This function can return the following errors:
+    ///
+    /// * `CoCClientError::MissingClientError` - If the client is not set in the `CoCClient` struct.
+    /// * `CoCClientError::Request` - If there was an error sending the HTTP request.
+    /// * `CoCClientError::Deserlisation` - If there was an error deserializing the response body.
+    /// * `CoCClientError::ClientError` - If the server responded with a client error (4xx status code).
+    /// * `CoCClientError::ServerError` - If the server responded with a server error (5xx status code).
+    /// * `CoCClientError::UnkownError` - If an unknown error occurred.
     pub async fn get_clan_information(
         self: Self,
-        clan_tag: String,
+        clan_tag: &str,
     ) -> Result<Clan, CoCClientError> {
         let encoded_clan_tag = encode(&clan_tag).into_owned();
 
