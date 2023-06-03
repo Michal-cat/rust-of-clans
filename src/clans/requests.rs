f34c34eb9494bf096d9d5243f6d3c65fbfe83c10
Mikkel Aas <aa.mikkel2902@gmail.com>
2023-06-03 13:03:16 +0200

    refactor folder structure

--- src/clans/requests.rs
+++ src/clans/requests.rs
@@ -1,79 +1,12 @@
-use crate::errors::{ClientError, CoCClientError, ServerError};
-use crate::models::{
-    CapitalRaidSeasons, Clan, ClanMembers, ClanWar, ClanWarLeagueGroup, ClanWarLog, Player,
-};
-use crate::CoCClient;
-use reqwest::StatusCode;
 use urlencoding::encode;
 
-pub struct ClientResponse {
-    pub text: String,
-    pub status_code: StatusCode,
-}
+use crate::{client::CoCClient, errors::CoCClientError};
+
+use super::models::{
+    CapitalRaidSeasons, Clan, ClanMembers, ClanWar, ClanWarLeagueGroup, ClanWarLog,
+};
 
 impl CoCClient {
-    /// Handles the response from the client and deserializes it into the specified type `T`.
-    ///
-    /// # Arguments
-    ///
-    /// * `client_response` - The response received from the HTTP client.
-    ///
-    /// # Returns
-    ///
-    /// Returns a `Result` containing the deserialized response as `T` on success,
-    /// or a `CoCClientError` if there was an error in deserialization or the response status code is unexpected.
-    async fn handle_response<T>(client_response: ClientResponse) -> Result<T, CoCClientError>
-    where
-        T: serde::de::DeserializeOwned,
-    {
-        match client_response.status_code {
-            status_code if status_code == StatusCode::OK => {
-                let result: T = serde_json::from_str(&client_response.text)
-                    .map_err(CoCClientError::Deserlisation)?;
-                Ok(result)
-            }
-            StatusCode::BAD_REQUEST
-            | StatusCode::FORBIDDEN
-            | StatusCode::NOT_FOUND
-            | StatusCode::TOO_MANY_REQUESTS => {
-                let error_body: ClientError = serde_json::from_str(&client_response.text)
-                    .map_err(CoCClientError::Deserlisation)?;
-                Err(CoCClientError::ClientError(error_body))
-            }
-            StatusCode::INTERNAL_SERVER_ERROR | StatusCode::SERVICE_UNAVAILABLE => {
-                let error_body: ServerError = serde_json::from_str(&client_response.text)
-                    .map_err(CoCClientError::Deserlisation)?;
-                Err(CoCClientError::ServerError(error_body))
-            }
-            _ => Err(CoCClientError::UnkownError),
-        }
-    }
-
-    /// Sends a GET request to the specified path and returns the client response.
-    ///
-    /// # Arguments
-    ///
-    /// * `path` - The path to which the GET request should be sent.
-    ///
-    /// # Returns
-    ///
-    /// Returns a `Result` containing the client response on success, or a `CoCClientError`
-    /// if there was an error sending the request or receiving the response.
-    async fn send_get_request(self, path: &str) -> Result<ClientResponse, CoCClientError> {
-        let client = self.client.ok_or(CoCClientError::MissingClientError)?;
-        let response = client
-            .get(path)
-            .send()
-            .await
-            .map_err(CoCClientError::Request)?;
-        let status_code = response.status();
-        let text = response.text().await.map_err(CoCClientError::Request)?;
-
-        let client_response = ClientResponse { text, status_code };
-
-        Ok(client_response)
-    }
-
     /// Retrieves clan information for the specified clan tag.
     ///
     /// # Arguments
@@ -215,19 +148,6 @@ impl CoCClient {
 
         CoCClient::handle_response(client_response).await
     }
-
-    pub async fn get_player_info(self: Self, player_tag: &str) -> Result<Player, CoCClientError> {
-        let encoded_player_tag = encode(&player_tag).into_owned();
-
-        let path = format!("{}/players/{}", self.url, encoded_player_tag);
-
-        let client_response = match self.send_get_request(&path).await {
-            Ok(client_response) => client_response,
-            Err(err) => return Err(err),
-        };
-
-        CoCClient::handle_response(client_response).await
-    }
 }
 
 #[cfg(test)]
@@ -235,8 +155,6 @@ mod tests {
     use super::*;
 
     static CLAN_TAG: &str = "#2LUGVU89Q";
-
-    static PLAYER_TAG: &str = "#2288UCQJ";
 
     fn set_up_client() -> CoCClient {
         let bearer_token = std::env::var("BEARER_TOKEN").expect("env var BEARER_TOKEN not set");
@@ -350,22 +268,6 @@ mod tests {
         let client = set_up_client();
 
         match client.get_clan_capital_raid_seasons(CLAN_TAG).await {
-            Ok(_) => {
-                assert!(true);
-            }
-            Err(err) => {
-                println!("{}", err);
-                assert!(false);
-            }
-        };
-    }
-
-    #[tokio::test]
-    #[ignore = "Bearer token is not configured for GitHub Actions IP address"]
-    async fn test_get_player_info() {
-        let client = set_up_client();
-
-        match client.get_player_info(PLAYER_TAG).await {
             Ok(_) => {
                 assert!(true);
             }
---
+++ src/players/requests.rs
@@ -0,0 +1,50 @@
+use crate::errors::CoCClientError;
+use urlencoding::encode;
+
+use crate::client::CoCClient;
+
+use super::models::Player;
+
+impl CoCClient {
+    pub async fn get_player_info(self: Self, player_tag: &str) -> Result<Player, CoCClientError> {
+        let encoded_player_tag = encode(&player_tag).into_owned();
+
+        let path = format!("{}/players/{}", self.url, encoded_player_tag);
+
+        let client_response = match self.send_get_request(&path).await {
+            Ok(client_response) => client_response,
+            Err(err) => return Err(err),
+        };
+
+        CoCClient::handle_response(client_response).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static PLAYER_TAG: &str = "#2288UCQJ";
+
+    fn set_up_client() -> CoCClient {
+        let bearer_token = std::env::var("BEARER_TOKEN").expect("env var BEARER_TOKEN not set");
+
+        CoCClient::new(bearer_token, None)
+    }
+
+    #[tokio::test]
+    #[ignore = "Bearer token is not configured for GitHub Actions IP address"]
+    async fn test_get_player_info() {
+        let client = set_up_client();
+
+        match client.get_player_info(PLAYER_TAG).await {
+            Ok(_) => {
+                assert!(true);
+            }
+            Err(err) => {
+                println!("{}", err);
+                assert!(false);
+            }
+        };
+    }
+}
