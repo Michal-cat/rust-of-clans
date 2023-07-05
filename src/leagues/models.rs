33a09ac11c5a5126a9fd8893dec5a45c8222e8e0
Mikkel Aas <aa.mikkel2902@gmail.com>
2023-07-05 20:27:06 +0200

    make league related models

--- src/leagues/models.rs
+++ src/leagues/models.rs
@@ -6,20 +6,60 @@ use crate::clans::models::Paging;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LeagueList<T> {
-    items: Vec<T>,
-    paging: Paging,
+    pub items: Vec<T>,
+    pub paging: Paging,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct CapitalLeague {
+pub struct SeasonList {
+    pub items: Vec<SeasonInfo>,
+    pub paging: Paging,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerRankingList {
+    pub items: Vec<PlayerRanking>,
+    pub paging: Paging,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PlayerRanking {
+    pub league: Option<League>,
+    pub clan: PlayerRankingClan,
+    pub attack_wins: i64,
+    pub defense_wins: i64,
+    pub tag: String,
     pub name: String,
-    pub id: i64,
+    pub exp_level: i64,
+    pub rank: i64,
+    pub previous_rank: Option<i64>,
+    pub trophies: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PlayerRankingClan {
+    pub tag: String,
+    pub name: String,
+    pub badge_urls: HashMap<String, String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct League {
     pub name: String,
-    pub id: i64,
+    pub id: u32,
     pub icon_urls: Option<HashMap<String, String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LeagueInfo {
+    pub id: u32,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SeasonInfo {
+    pub id: String,
 }
